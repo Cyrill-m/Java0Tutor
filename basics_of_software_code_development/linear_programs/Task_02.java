91cb9d3017896e4913fcbcacc1084de000547101
Cyrill M <45451180+Cyrill-m@users.noreply.github.com>
2019-07-17 17:16:35 +0300

    Update Task_02.java

--- basics_of_software_code_development/linear_programs/Task_02.java
+++ basics_of_software_code_development/linear_programs/Task_02.java
@@ -47,6 +47,7 @@ public class Task_02 {
         System.out.println("Введите значение c:");
         if (input.hasNextDouble()){
             c = input.nextDouble();
+            input.close();
         } else {
             System.out.println("Ошибка ввода! Перезапустите программу.");
             input.close();
@@ -56,19 +57,16 @@ public class Task_02 {
         resultSqrt = b * b + 4 * a * c;
         if (resultSqrt < 0){
             System.out.println("Ошибка: корень из отрицательного числа. Перезапустите программу.");
-            input.close();
             return;
         }
         
         resultZnam = 2 * a;
         if (resultZnam == 0){
-            System.out.println("Ошибка: деление на ноль. Перезапустите программу.");
-            input.close();
+            System.out.println("Ошибка: деление на ноль. Перезапустите программу.");            
             return;
         }
         
         result = (b + Math.sqrt(resultSqrt)) / resultZnam - Math.pow(a, 3) * c + Math.pow(b, -2) ;
-        System.out.printf("Значение функции = %.3f \n", result);
-        input.close();
+        System.out.printf("Значение функции = %.3f \n", result);        
     }    
 }
