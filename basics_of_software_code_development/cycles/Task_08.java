3e527664c247d32f340c254965de8a380d113e9d
Cyrill M <45451180+Cyrill-m@users.noreply.github.com>
2019-11-19 20:30:26 +0300

    Update Task_08.java

--- basics_of_software_code_development/cycles/Task_08.java
+++ basics_of_software_code_development/cycles/Task_08.java
@@ -1,14 +1,54 @@
 package basics_of_software_code_development.cycles;
 
+import java.util.HashSet;
+
 /**
  *
  * @author Kiryl Matusevich
  * 
- * ЦИКЛЫ
- * Задача 8 - Даны два числа. Определить цифры, 
- * входящие в запись как первого так и второго числа.
+ * Р¦РРљР›Р«
+ * Р—Р°РґР°С‡Р° 8 - Р”Р°РЅС‹ РґРІР° С‡РёСЃР»Р°. РћРїСЂРµРґРµР»РёС‚СЊ С†РёС„СЂС‹, 
+ * РІС…РѕРґСЏС‰РёРµ РІ Р·Р°РїРёСЃСЊ РєР°Рє РїРµСЂРІРѕРіРѕ С‚Р°Рє Рё РІС‚РѕСЂРѕРіРѕ С‡РёСЃР»Р°.
  */
 
 public class Task_08 {
     
+    public static void main(String[] args) {
+        
+        int num1;
+        int num2;
+        HashSet<Integer> digSet; //РјРЅРѕР¶РµСЃС‚РІРѕ РѕР±С‰РёС… С†РёС„СЂ
+        
+        num1 = 12422;
+        num2 = 12911;
+        digSet = new HashSet();
+        
+        System.out.println("Р”Р°РЅС‹ С‡РёСЃР»Р° " + num1 + " Рё " + num2);
+        System.out.print("РћР±С‰РёРµ С†РёС„СЂС‹: ");
+        
+        while(num1 > 0){
+            int dig1;
+            dig1 = num1 % 10;
+            num1 = num1 / 10;
+            int num2t = num2;
+            
+            while(num2t > 0){
+                int dig2;
+                dig2 = num2t % 10;
+                num2t = num2t / 10;
+                if (dig1 == dig2){
+                    digSet.add(dig1);
+                    break;
+                }
+            }
+        }
+        
+        if (digSet.isEmpty()) {
+            System.out.print("РЅРµС‚ РѕР±С‰РёС… С†РёС„СЂ");
+        } else {
+            System.out.print(digSet);
+        }
+        
+        System.out.println();
+    }    
 }
