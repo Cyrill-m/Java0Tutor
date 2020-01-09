7df340ee37578b6c43af7907120ced346703734e
Cyrill M <45451180+Cyrill-m@users.noreply.github.com>
2020-01-09 13:19:02 +0300

    Update Task_15.java

--- algorithmization/multidimensional_arrays/Task_15.java
+++ algorithmization/multidimensional_arrays/Task_15.java
@@ -1,16 +1,60 @@
 package algorithmization.multidimensional_arrays;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  *
  * @author Kiryl Matusevich
  * 
- * МАССИВЫ МАССИВОВ
- * Задача 15 - Найдите наибольший элемент матрицы и заменить 
- * все нечетные элементы на него.
+ * РњРђРЎРЎРР’Р« РњРђРЎРЎРР’РћР’
+ * Р—Р°РґР°С‡Р° 15 - РќР°Р№РґРёС‚Рµ РЅР°РёР±РѕР»СЊС€РёР№ СЌР»РµРјРµРЅС‚ РјР°С‚СЂРёС†С‹ Рё Р·Р°РјРµРЅРёС‚СЊ 
+ * РІСЃРµ РЅРµС‡РµС‚РЅС‹Рµ СЌР»РµРјРµРЅС‚С‹ РЅР° РЅРµРіРѕ.
  */
 
 public class Task_15 {
     
+    public static void main(String[] args) {
+        int[][] a;
+        int n, m;
+        int max;
+        Random rand;
+        
+        n = 5;
+        m = 8;
+        a = new int[n][m];
+        rand = new Random();
+        
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a[i].length; j++){
+                a[i][j] = rand.nextInt(20);
+            }
+        }
+        
+        for(int[] a1: a){
+            System.out.println(Arrays.toString(a1));
+        }
+        
+        max = a[0][0];
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a[i].length; j++){
+                if(max < a[i][j]){
+                    max = a[i][j];
+                }                
+            }
+        }
+        
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a[i].length; j++){
+                if(a[i][j] % 2 != 0){
+                    a[i][j] = max;
+                }                
+            }
+        }
+        
+        System.out.println("max = " + max);
+        for(int[] a1: a){
+            System.out.println(Arrays.toString(a1));
+        }
+    }
 }
